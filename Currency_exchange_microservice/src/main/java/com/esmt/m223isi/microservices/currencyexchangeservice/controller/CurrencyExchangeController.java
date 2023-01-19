af9635f028e03c3de1cd6851d2c78436eb4f9b37
Libasse Laye <78869773+Libasse-coder@users.noreply.github.com>
2023-01-20 00:15:36 +1000

    Add files via upload

---
+++ Currency_exchange_microservice/src/main/java/com/esmt/m223isi/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -0,0 +1,31 @@
+package com.esmt.m223isi.microservices.currencyexchangeservice.controller;
+
+import java.math.BigDecimal;
+
+import org.apache.catalina.startup.ClassLoaderFactory.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.esmt.m223isi.microservices.currencyexchangeservice.bean.CurrencyExchange;
+import com.esmt.m223isi.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
+
+@RestController
+public class CurrencyExchangeController {
+
+	@Autowired
+	CurrencyExchangeRepository repository;
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange getCurrencyExchange (@PathVariable String from, @PathVariable String to) {
+		
+		//return new CurrencyExchange(1001L, from, to, BigDecimal.valueOf(609.5));
+		
+		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+		if(currencyExchange==null)
+			throw new RuntimeException("Taux de change non trouvé pour " + from + "--" + to);
+		
+		return currencyExchange;
+	}
+}
